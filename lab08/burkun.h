a56fa6f1b1192addb3484dbe816a47ce467bc2a8
Stanislav Burkun <114885551+sburkun@users.noreply.github.com>
2023-03-13 13:53:51 +0200

    Завантажено код завдання 8.1 та 8.2

---
+++ lab08/burkun.h
@@ -0,0 +1,6 @@
+#ifndef BURKUN_H_INCLUDED
+#define BURKUN_H_INCLUDED
+
+int s_calculation(int x,int y,int z,int s);
+
+#endif // BURKUN_H_INCLUDED
---
+++ lab08/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "D:\LAB8\ModulesBurkun\burkun.h"
+
+using namespace std;
+
+int main(){
+    int x, y, z, s;
+
+    cout << "Enter x:";
+    cin >> x;
+    cout << "Enter y:";
+    cin >> y;
+    cout << "Enter z:";
+    cin >> z;
+
+    cout << s_calculation(x,y,z,s);
+    return 0;
+
+}
