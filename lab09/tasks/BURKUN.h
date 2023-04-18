2e1a0b20ea199333f7793122584540f6b8d56d1f
Stanislav Burkun <114885551+sburkun@users.noreply.github.com>
2023-04-18 23:04:15 +0200

    Завдання 9.1-9.3

---
+++ lab09/tasks/BURKUN.h
@@ -0,0 +1,12 @@
+#ifndef BURKUN_H_INCLUDED
+#define BURKUN_H_INCLUDED
+
+int s_calculation(int x,int y,int z,int s);
+
+int binarny_chisla();
+
+int foot_size(double footLength);
+
+int ocinka(int rc1, int rc2, int sem);
+
+#endif // BURKUN_H_INCLUDED
